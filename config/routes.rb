68586410576ccde5e5e98c0dbce179d74da8e1eb
Sebastien Neyt <bas_neyt@hotmail.com>
2022-03-01 15:10:29 +0100

    authorization, calendar_list and events list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get '/redirect', to: 'google_api#redirect', as: 'redirect'
+  get '/callback', to: 'google_api#callback', as: 'callback'
+  get '/dashboard_events', to: 'google_api#dashboard_events', as: 'dashboard'
+  get '/calendars', to: 'google_api#calendars', as: 'calendars'
+
   devise_for :users
   root to: 'pages#home'
-  get '/dashboard', to: 'pages#dashboard'
+  # get '/dashboard', to: 'pages#dashboard'
   resources :users do
     resources :events
   end
