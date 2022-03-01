ed18408976db69e3769757843664086fa06017c0
Arno Debelle <debelle.a@gmail.com>
2022-03-01 20:25:50 +0100

    creating view for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,47 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  def index
+    @user = User.find(params[:user_id])
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    @event.save
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Your meeting has been successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path, notice: "Your meeting was successfully deleted."
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:start_at, :end_at, :description, :duration)
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+  has_many :user_events, dependent: :destroy
   has_many :users, through: :user_events
   has_many :slots, dependent: :destroy
   belongs_to :user
